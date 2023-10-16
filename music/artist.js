d381d6a2061ca03cd75d0a2f31207ff73efeb1c7
SakuyaYae <darkreimu@outlook.com>
2023-10-16 10:09:39 +0200

    chnged properties to private
    also added artist name getter

--- music/artist.js
+++ music/artist.js
@@ -1,18 +1,18 @@
 export default class Artist {
-  artistName;
-  artistInfo;
-  yearBorn;
-  currentBands;
-  previusBands;
-  intrumentsPlayed;
+  #artistName;
+  #artistInfo;
+  #yearBorn;
+  #currentBands;
+  #previusBands;
+  #intrumentsPlayed;
 
   constructor(artistName, artistInfo, yearBorn, intrumentsPlayed, currentBands = [], previusBands = []) {
-    this.artistName = artistName;
-    this.artistInfo = artistInfo;
-    this.yearBorn = yearBorn;
-    this.currentBands = currentBands;
-    this.previusBands = previusBands;
-    this.intrumentsPlayed = intrumentsPlayed;
+    this.#artistName = artistName;
+    this.#artistInfo = artistInfo;
+    this.#yearBorn = yearBorn;
+    this.#currentBands = currentBands;
+    this.#previusBands = previusBands;
+    this.#intrumentsPlayed = intrumentsPlayed;
   }
 
   addBand() {
@@ -24,17 +24,21 @@ export default class Artist {
   }
 
   displayArtist() {
-    console.log(` Name: ${this.artistName}
-                  About: ${this.artistInfo}
-                  Age: ${this.#calculateAge()} Born: ${this.yearBorn}
-                  Current bands: ${this.currentBands}
-                  Previus bands: ${this.previusBands}
-                  Instruments ${this.intrumentsPlayed}`)
+    console.log(` Name: ${this.#artistName}
+                  About: ${this.#artistInfo}
+                  Age: ${this.#calculateAge()} Born: ${this.#yearBorn}
+                  Current bands: ${this.#currentBands}
+                  Previus bands: ${this.#previusBands}
+                  Instruments ${this.#intrumentsPlayed}`)
   }
 
   #calculateAge() {
     const currentYear = parseInt(Date().substring(11, 16).trim());
-    const age = currentYear - this.yearBorn;
+    const age = currentYear - this.#yearBorn;
     return age;
+  }
+
+  get artistName() {
+    return this.#artistName;
   }
 }
