b7542d05edb5498a0849499b611819e8801d9f11
SakuyaYae <darkreimu@outlook.com>
2023-10-16 13:28:57 +0200

    Update band.js
    Added get methods for private properties and changed functionalety to make use of utils class

--- music/band.js
+++ music/band.js
@@ -1,3 +1,6 @@
+import Display from "./display.js";
+import Utils from "./utils.js";
+
 export default class Band {
   #bandName;
   #bandInfo;
@@ -6,40 +9,37 @@ export default class Band {
   #currentMembers;
   #previusMembers;
 
-  constructor(bandName, bandInfo, yearFounded, currentMembers, yearDisolved = "") {
+  constructor(bandName = "", bandInfo = "", yearFounded = "", currentMembers = [], yearDisolved = "") {
     this.#bandName = bandName;
     this.#bandInfo = bandInfo;
     this.#yearFounded = yearFounded;
     this.#yearDisolved = yearDisolved;
     this.#currentMembers = currentMembers;
+    this.display = new Display();
+    this.utils = new Utils();
   }
 
-  addArtist() {
-
-  }
-
-  removeArtist() {
-
-  }
-
-  displayBand() {
-    if (this.#yearDisolved === "") {
-      console.log(` Name: ${this.#bandName}
-                  About: ${this.#bandInfo}
-                  Founded: ${this.#yearFounded}
-                  Current Members: ${this.#currentMembers}
-                  Previus Members: ${this.#previusMembers}`);
-    }
-    else {
-      console.log(` Name: ${this.#bandName}
-                  About: ${this.#bandInfo}
-                  Founded: ${this.#yearFounded}
-                  Disolved: ${this.#yearDisolved}
-                  Current Members: ${this.#currentMembers}
-                  Previus Members: ${this.#previusMembers}`);
-    }
-  }
   getBandName() {
     return this.#bandName;
+  }
+
+  getBandInfo() {
+    return this.#bandInfo;
+  }
+
+  getYearFounded() {
+    return this.#yearFounded;
+  }
+
+  getYearDisolved() {
+    return this.#yearDisolved;
+  }
+
+  getCurrentMembers() {
+    return this.#currentMembers;
+  }
+
+  getPreviusMembers() {
+    return this.#previusMembers;
   }
 }
