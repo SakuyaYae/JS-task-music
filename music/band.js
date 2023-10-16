12425d0df8721ae85005fd48b1c90adb9c113001
SakuyaYae <darkreimu@outlook.com>
2023-10-16 10:40:50 +0200

    Update band.js
    changed properties to private and implemented displayBand and getBandName

--- music/band.js
+++ music/band.js
@@ -1,26 +1,45 @@
 export default class Band {
-  bandName;
-  bandInfo;
-  yearFounded;
-  yearDisolved;
-  currentMembers;
-  previusMembers;
+  #bandName;
+  #bandInfo;
+  #yearFounded;
+  #yearDisolved;
+  #currentMembers;
+  #previusMembers;
 
-  constructor(bandName, bandInfo, yearFounded, yearDisolved = "", currentMembers) {
-    this.bandName = bandName;
-    this.bandInfo = bandInfo;
-    this.yearFounded = yearFounded;
-    this.yearDisolved = yearDisolved;
-    this.currentMembers = currentMembers;
+  constructor(bandName, bandInfo, yearFounded, currentMembers, yearDisolved = "") {
+    this.#bandName = bandName;
+    this.#bandInfo = bandInfo;
+    this.#yearFounded = yearFounded;
+    this.#yearDisolved = yearDisolved;
+    this.#currentMembers = currentMembers;
   }
 
   addArtist() {
 
   }
+
   removeArtist() {
 
   }
-  displayBand() {
 
+  displayBand() {
+    if (this.#yearDisolved === "") {
+      console.log(` Name: ${this.#bandName}
+                  About: ${this.#bandInfo}
+                  Founded: ${this.#yearFounded}
+                  Current Members: ${this.#currentMembers}
+                  Previus Members: ${this.#previusMembers}`);
+    }
+    else {
+      console.log(` Name: ${this.#bandName}
+                  About: ${this.#bandInfo}
+                  Founded: ${this.#yearFounded}
+                  Disolved: ${this.#yearDisolved}
+                  Current Members: ${this.#currentMembers}
+                  Previus Members: ${this.#previusMembers}`);
+    }
+  }
+  getBandName() {
+    return this.#bandName;
   }
 }
