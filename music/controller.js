840f59142804f78856ce0bdbb127a16b6842aa30
SakuyaYae <darkreimu@outlook.com>
2023-10-17 15:24:51 +0200

    Create controller.js

---
+++ music/controller.js
@@ -0,0 +1,26 @@
+import bandUtils from "./bandUtils.js";
+import ArtistUtils from "./artistUtils.js";
+
+export default class Controller {
+  constructor() { }
+
+  addNew(objectToCreate) {
+
+  }
+
+  deleteObject(objectToDelete) {
+
+  }
+
+  addTo(objectToAdd) {
+
+  }
+
+  removeFrom(objectToRemove) {
+
+  }
+
+  displayData(objectToDisplay) {
+
+  }
+}
