94ee1106bfa0038bd26a53d72fe1988acbf72362
SakuyaYae <darkreimu@outlook.com>
2023-10-17 15:26:17 +0200

    Update artistUtils.js
    added functionalety

--- music/artistUtils.js
+++ music/artistUtils.js
@@ -13,12 +13,33 @@ export default class ArtistUtils {
   }
 
   removeArtist(indexToRemove) {
-    this.#artistList.splice(indexToRemove, 0);
+    this.#artistList.splice(indexToRemove, 1);
+  }
+
+
+  addBandToArtist(band, indexOfArtist) {
+    this.#artistList[indexOfArtist].setCurrentMembers(band);
+  }
+
+  removeBandFromArtist(band, indexOfArtist) {
+    const bands = this.#artistList[indexOfArtist].getCurrentBands();
+    const indexOfBand = bands.indexOf(band);
+    const previusBand = bands.splice(indexOfBand, 1);
+    this.#artistList[indexOfArtist].setCurrentBands(bands, 1);
+    this.#artistList[indexOfArtist].setPreviusBands(previusBand);
   }
 
   display() {
-    for (var i; i < this.#artistList; i++) {
-      console.log(this.#artistList[i]);
+    if (this.#artistList.length > 0) {
+      for (var i; i < this.#artistList; i++) {
+        console.log(this.#artistList[i]);
+      }
+    }
+    else if (this.#artistList.length === 0) {
+      console.log("there are no artists to display");
+    }
+    else {
+      console.log("An error has happend");
     }
   }
 }
