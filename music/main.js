c0193462a2cdbf9326822b92d20302797427c8a8
SakuyaYae <darkreimu@outlook.com>
2023-10-20 16:04:04 +0200

    Update main.js
    changed to run comand loop from main function and added call to controller.writeToJson()

--- music/main.js
+++ music/main.js
@@ -22,64 +22,68 @@ function displayMenu() {
       100. Quit the program`
   );
 }
+function main() {
 
-displayMenu();
-while (run) {
-  switch (parseInt(userInput().trim())) {
-    case 1:
-      displayMenu();
-      break;
 
-    case 2:
-      controller.addNew("band");
-      console.log("Band created");
-      break;
+  displayMenu();
+  while (run) {
+    switch (parseInt(userInput().trim())) {
+      case 1:
+        displayMenu();
+        break;
 
-    case 3:
-      controller.addNew("artist");
-      console.log("Artist created");
-      break;
+      case 2:
+        controller.addNew("band");
+        console.log("Band created");
+        break;
 
-    case 4:
-      controller.deleteObject("band");
-      break;
+      case 3:
+        controller.addNew("artist");
+        console.log("Artist created");
+        break;
 
-    case 5:
-      controller.deleteObject("artist");
-      break;
+      case 4:
+        controller.deleteObject("band");
+        break;
 
-    case 6:
-      controller.displayData("band");
-      break;
+      case 5:
+        controller.deleteObject("artist");
+        break;
 
-    case 7:
-      controller.displayData("artist");
-      break;
+      case 6:
+        controller.displayData("band");
+        break;
 
-    case 8:
-      controller.addTo("band");
-      break;
+      case 7:
+        controller.displayData("artist");
+        break;
 
-    case 9:
-      controller.addTo("artist")
-      break;
-    case 10:
-      controller.removeFrom("band");
-      break;
+      case 8:
+        controller.addTo("band");
+        break;
 
-    case 11:
-      controller.removeFrom("artist");
-      break;
+      case 9:
+        controller.addTo("artist")
+        break;
+      case 10:
+        controller.removeFrom("band");
+        break;
 
-    case 100:
-      run = false;
-      console.log("Program has been quit.");
-      break;
+      case 11:
+        controller.removeFrom("artist");
+        break;
 
-    default:
-      console.log("Invalid input. type a valid number from the menu.");
-      displayMenu();
-      break;
+      case 100:
+        run = false;
+        console.log("Program has been quit.");
+        break;
+
+      default:
+        console.log("Invalid input. type a valid number from the menu.");
+        displayMenu();
+        break;
+    }
   }
+  controller.writeToJson();
 }
-
+main();
